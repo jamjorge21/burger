1b2ecff081ba6d13c8b25f81418689c153739c26
Jorge Martinez <70128888+jamjorge21@users.noreply.github.com>
2021-01-23 08:52:52 -0800

    Update burgers_controllers.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,31 +1,52 @@
 var express = require("express");
+var burger = require("../models/burger");
 
 var router = express.Router();
 
-
-
-var burger = require("../models/burger.js");
-
 router.get("/", function(req, res) {
-    burger.all(function(burger_data) {
-        console.log(burger_data);
-        res.render("index", {burger_data});
-    });  
-});
+  burger.selectAll(function(data) {
+    var hdbrsObj = {
+      burgers: data
+    };
+    console.log(hdbrsObj);
+    res.render("index", hdbrsObj);
+  });
 
-router.put("/burgers/update", function(req, res) {
-    burger.update(req.body.burger_id, function(result) {
-        console.log(result);
-        res.redirect("/");
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne(
+      ["burger_name", "devoured"],
+      [req.body.burger_name, req.body.devoured],
+      function(result) {
+        // Send back the ID of new burger
+        res.json({ id: result.insertId });
+      }
+    );
+  });
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(
+      result
+    ) {
+      if ((result.changedRows === 0)) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     });
+  });
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.deleteOne(condition, function(result) {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 });
-
-
-router.post("/api/burgers", function(req, res) {
-    burger.create(req.body.burger_name, function(result) {
-        res.redirect("/");
-    })
-})
-
-
 module.exports = router;
