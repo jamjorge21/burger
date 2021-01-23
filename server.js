64d2b1e7e6c046cab77bad68c7485c304f15da84
Jorge Martinez <70128888+jamjorge21@users.noreply.github.com>
2021-01-23 08:47:11 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 var express = require("express");
-var path = require ("path");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-var app = express ();
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
-
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controllers");
-app.use("/", routes);
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
-var port = process.env.PORT || 8080;
-app.listen(port);
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
