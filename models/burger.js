592a59d157328a5a2d874f91717884b30cc24cf1
Jorge Martinez <70128888+jamjorge21@users.noreply.github.com>
2021-01-23 08:49:45 -0800

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,29 @@
+  
 var orm = require("../config/orm.js");
-
-
 var burger = {
-    all: function(cb) {
-        orm.all("burgers", function(res) {
-            cb(res);
-        });
-    },
-    create: function(name, cb) {
-        orm.create("burgers", name, cb);
-    },
-    update: function(id, cb) {
-        orm.update("burgers", id, cb);
-    }
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
 module.exports = burger;
